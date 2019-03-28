4910e26b543127d47aaa0e362d5912460d5be98f
BIMiracle <BIMiracle>
2019-03-28 11:14:21 +0800

    添加网络状态和颜色,添加网络状态筛选,添加Log信息颜色

---
+++ LLDebugToolDemo/DemoViewControllers/TestLogViewController.m
@@ -0,0 +1,73 @@
+//
+//  TestLogViewController.m
+//  LLDebugToolDemo
+//
+//  Created by admin10000 on 2018/8/29.
+//  Copyright © 2018年 li. All rights reserved.
+//
+
+#import "TestLogViewController.h"
+#import "LLDebugToolMacros.h"
+
+@interface TestLogViewController ()
+
+@end
+
+@implementation TestLogViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = NSLocalizedString(@"test.log", nil);
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [super tableView:tableView cellForRowAtIndexPath:indexPath];
+    if (indexPath.row == 0) {
+        cell.textLabel.text = NSLocalizedString(@"insert.log", nil);
+    } else if (indexPath.row == 1) {
+        cell.textLabel.text = NSLocalizedString(@"insert.error.log", nil);
+    } else if (indexPath.row == 2) {
+        cell.textLabel.text = NSLocalizedString(@"insert.call.log", nil);
+    }
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.row == 0) {
+        [self testNormalLog];
+    } else if (indexPath.row == 1) {
+        [self testErrorLog];
+    } else if (indexPath.row == 2) {
+        [self testEventLog];
+    }
+}
+
+#pragma mark - Actions
+- (void)testNormalLog {
+//    LLog(NSLocalizedString(@"normal.log.info", nil));
+    LLog(@"一条普通的信息");
+    LLog_Alert(@"alert消息");
+    LLog_Alert_Event(@"点击了Button", @"alert信息");
+    LLog_Warning(@"一条警告消息");
+    LLog_Warning_Event(@"点击了Label", @"iajsid");
+    LLog_Error(@"一条Error消息");
+    LLog_Error_Event(@"刷新崩溃了", @"asdasidj");
+    [[LLDebugTool sharedTool] showDebugViewControllerWithIndex:1];
+}
+
+- (void)testErrorLog {
+    LLog_Error(NSLocalizedString(@"error.log.info", nil));
+    [[LLDebugTool sharedTool] showDebugViewControllerWithIndex:1];
+}
+
+- (void)testEventLog {
+    LLog_Error_Event(NSLocalizedString(@"call", nil),NSLocalizedString(@"call.log.info", nil));
+    [[LLDebugTool sharedTool] showDebugViewControllerWithIndex:1];
+}
+
+@end
